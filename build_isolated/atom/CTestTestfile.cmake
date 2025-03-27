a69c1f2f7cb864b65b6fbbb92ad219ce9e4c4748
NguyenChiCuong-1706 <145039684+NguyenChiCuong-1706@users.noreply.github.com>
2025-03-27 15:50:24 +0700

    Add files via upload

---
+++ build_isolated/atom/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/chicuong/ardupilot_ws/src/atom
+# Build directory: /home/chicuong/ardupilot_ws/build_isolated/atom
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build_isolated/atom/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/chicuong/ardupilot_ws/devel_isolated/atom:$CMAKE_PREFIX_PATH"
+export PWD='/home/chicuong/ardupilot_ws/build_isolated/atom'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/chicuong/ardupilot_ws/devel_isolated/atom/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/chicuong/ardupilot_ws/src/atom:$ROS_PACKAGE_PATH"
